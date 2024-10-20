634c250e3e6917d7709a876f2374f3e1a93ca612
vanshika1807 <67184259+vanshika1807@users.noreply.github.com>
2024-10-20 15:39:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const countdown = () => {
+    const countDate = new Date('Dec 31, 2024 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(gap / day);
+    const hours = Math.floor((gap % day) / hour);
+    const minutes = Math.floor((gap % hour) / minute);
+    const seconds = Math.floor((gap % minute) / second);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (gap < 0) {
+        document.getElementById('countdown').innerText = "We're Live!";
+    }
+};
+
+setInterval(countdown, 1000);
